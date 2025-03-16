407a647ecd1c711f7a0c3f2ad5f35ec9ab0db542
Ozioma <victoryehiemenonye@gmail.com>
2025-03-16 22:18:54 +0100

    feat: added ambanlance new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Doctors from './pages/Doctors'
+import Emergency from './pages/Emergency'
 import Login from './pages/Login'
 import About from './pages/About'
 import Contact from './pages/Contact'
@@ -23,6 +24,7 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/doctors' element={<Doctors />} />
         <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/emergency' element={<Emergency />} />
         <Route path='/login' element={<Login />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
