c6f47595a27f4e564eda890261026884805345d5
Annytech001 <godlybassey101@gmail.com>
2025-03-16 21:05:51 +0100

    footer

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-[#ADADAD]'>
       <img onClick={() => navigate('/')} className='w-24 cursor-pointer' src={assets.logo} alt="logoimage" />
-      <ul className='md:flex items-start gap-5 font-medium hidden'>
+      <ul className='md:flex items-start gap-3 font-medium hidden'>
         <NavLink to='/' >
           <li className='py-1'>HOME</li>
           <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
