3320a1f24e0d1c368367ae6c5f29c5060b47cb1f
Ozioma <victoryehiemenonye@gmail.com>
2025-03-16 18:08:48 +0100

    newbranch

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
       <ul className='md:flex items-start gap-5 font-medium hidden'>
         <NavLink to='/' >
           <li className='py-1'>HOME</li>
-          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />  
         </NavLink>
         <NavLink to='/doctors' >
           <li className='py-1'>ALL DOCTORS</li>
